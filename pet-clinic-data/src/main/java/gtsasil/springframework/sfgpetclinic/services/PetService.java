17a75a4f9560000649d48a47b433218b2b80d4e9
Gleisson Santos Silva <gtsasil@gmail.com>
2020-01-06 12:16:04 +0000

    Implement Interface for service

---
+++ pet-clinic-data/src/main/java/gtsasil/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package gtsasil.springframework.sfgpetclinic.services;
+
+import gtsasil.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findByLastName(String lastName);
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/gtsasil/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package gtsasil.springframework.sfgpetclinic.services;
+
+import gtsasil.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
