151a351f36115827623169f73afd03259699bd5a
Gleisson Santos Silva <gtsasil@gmail.com>
2020-01-09 12:51:37 +0000

    adding owner page and controller. closes #13

---
+++ pet-clinic-web/src/main/java/gtsasil/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package gtsasil.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners","/owner/index","/owner/index.html" })
+    public String listOwners(){
+        return "owners/index";
+    }
+
+
+}
